5b24a080744d8e613fae5b13a2451556092abac5
Abednego Alex <abednegoalex1@gmail.com>
2025-08-15 10:37:31 +0100

    added fix for github action

--- server.js
+++ server.js
@@ -85,12 +85,6 @@ app.get('/', (req, res) => {
   res.send('Welcome to GIFTY CAKES API');
 });
 
-app.get('/ping', (req, res) => {
-  console.log(`Ping from cron-job.org at ${new Date().toISOString()}`);
-  res.sendStatus(200); 
-});
-
-
 
 // User Management 
 
